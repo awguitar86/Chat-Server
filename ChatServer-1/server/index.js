beb21d7d95065550dba0998dcf8a6375d498f2de
Austin Wright <awguitar86@gmail.com>
2018-02-12 14:04:29 -0700

    required express and body-parser, created an express app, configured app to parse JSON from body, and configured app to listne on port 3000

---
+++ ChatServer-1/server/index.js
@@ -0,0 +1,10 @@
+var express = require('express');  //this is how to import/require things in a node server.
+var bodyParser = require('body-parser');
+
+var app = express();  //this creates an express app by saving it to a variable.
+
+app.use( bodyParser.json() );  //this configures the express app to parse JSON from the body.
+
+const port = 3000;  //port is saved to a variable in case it needs changed, it only needs changed once.
+app.listen( port, () => { console.log(`Server is listening on port ${port}`); } );  //this uses the listen method from express to let the app know what port to listen on. 
+
