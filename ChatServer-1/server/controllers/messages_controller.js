abd8b6c37db78ed3e6d2ff3fdff8bd78e225953e
Austin Wright <awguitar86@gmail.com>
2018-02-12 14:11:17 -0700

    exported an object with methods create, read, update, and delete with parameters req, and res.

--- ChatServer-1/server/controllers/messages_controller.js
+++ ChatServer-1/server/controllers/messages_controller.js
@@ -1,4 +1,17 @@
 var messages = [];
 var id = 0;
 
+module.exports = {
+    create: ( req, res ) => {
 
+    },
+    read: ( req, res ) => {
+
+    },
+    update: ( req, res ) => {
+
+    },
+    delete: ( req, res ) => {
+        
+    }
+}
