4808a9ea5a2885da6a86af6f404e57a679bf6544
Austin Wright <awguitar86@gmail.com>
2018-02-12 16:07:50 -0700

    created controller and hooked up to index.js

--- ChatServer-2/server/index.js
+++ ChatServer-2/server/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mc = require( __dirname + '/controllers/messages_controller');
 
 var app = express();
 
 app.use( bodyParser.json() );
+app.use( express.static( __dirname + '/../public/build' ) ); 
 
-var port = 3000;
+const messagesBaseUrl = '/api/messages';
+app.post( messagesBaseUrl, mc.create );
+app.get( messagesBaseUrl, mc.read );
+app.put( `${messagesBaseUrl}/:id`, mc.update );
+app.delete( `${messagesBaseUrl}/:id`, mc.delete );
+
+const port = 3000;
 app.listen( port, () => { console.log( `Server is listenign on port ${port}`); });
 
