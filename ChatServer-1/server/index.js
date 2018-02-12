989441c26019bda8bd02e7f8a828be2b27ea6c04
Austin Wright <awguitar86@gmail.com>
2018-02-12 14:52:18 -0700

    created post, get, put, and delete endpoints, and notes for all of them.

--- ChatServer-1/server/index.js
+++ ChatServer-1/server/index.js
@@ -1,12 +1,17 @@
-var express = require('express');  //this is how to import/require things in a node server.
+var express = require('express');                       //this is how to import/require things in a node server.
 var bodyParser = require('body-parser');
 var mc = require( __dirname + '/controllers/messages_controller' );
 
-var app = express();  //this creates an express app by saving it to a variable.
+var app = express();                                    //this creates an express app by saving it to a variable.
 
-app.use( bodyParser.json() );  //this configures the express app to parse JSON from the body.
+app.use( bodyParser.json() );                           //this configures the express app to parse JSON from the body.
 
-const port = 3000;  //port is saved to a variable in case it needs changed, it only needs changed once.
+const port = 3000;                                      //port is saved to a variable in case it needs changed, it only needs changed once.
 app.listen( port, () => { console.log(`Server is listening on port ${port}`); } );  //this uses the listen method from express to let the app know what port to listen on. 
 
-const messagesBaseUrl = "/api/messages";  //make a messagesBaseUrl variable so that if the URL ever changes we won't have to update in four different places.
+const messagesBaseUrl = "/api/messages";                //make a messagesBaseUrl variable so that if the URL ever changes we won't have to update in four different places.
+app.post( messagesBaseUrl, mc.create );                 //We can then use the built-in methods express gives us to create endpoints. 
+app.get( messagesBaseUrl, ms.read );                    //We'll use post for create; get for read; put for update; and delete for delete.
+app.put( `${messagesBaseUrl}/:id`, mc.update );         //For the put and delete endpoints, we need to add on a url parameter of id.
+app.delete( `${messagesBaseUrl}/:id`, mc.delete );      //A url paramter can be defined by adding :variableName when making the URL for an endpoint.
+
