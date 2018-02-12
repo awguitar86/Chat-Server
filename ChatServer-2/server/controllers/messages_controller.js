2778a05f0cd9d3bc5f8527bdac5d8f52fd20228e
Austin Wright <awguitar86@gmail.com>
2018-02-12 15:56:39 -0700

    exported create, read, update, and delete methods

--- ChatServer-2/server/controllers/messages_controller.js
+++ ChatServer-2/server/controllers/messages_controller.js
@@ -1,3 +1,20 @@
 var messages = [];
 var id = 0;
 
+module.exports = {
+    create: ( req, res ) => {
+        const { text, time } = req.body;
+        messages.push({ id, text, time });
+        id++;
+        res.status(200).send( messages );
+    },
+    read: ( req, res ) => {
+        res.status(200).send( messages );
+    },
+    update: ( req, res ) => {
+
+    },
+    delete: ( req, res ) => {
+
+    }
+}
