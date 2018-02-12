5188cc5e2746c4bcfb42f998bdd5947222384bbd
Austin Wright <awguitar86@gmail.com>
2018-02-12 14:12:46 -0700

    added notes

--- ChatServer-1/server/controllers/messages_controller.js
+++ ChatServer-1/server/controllers/messages_controller.js
@@ -1,7 +1,7 @@
-var messages = [];
-var id = 0;
+var messages = [];  //Create an array to hold the messages.
+var id = 0;         //Create a variable that will keep track of what id to assign to messages. The id should start at 0 and increment after every creation.
 
-module.exports = {
+module.exports = {  //Export an object with methods to create, read, update, and delete messages. Each with two prameters req and res.
     create: ( req, res ) => {
 
     },
@@ -12,6 +12,6 @@ module.exports = {
 
     },
     delete: ( req, res ) => {
-        
+
     }
 }
