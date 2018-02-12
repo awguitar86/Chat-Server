0c5feac675212c36c0169c7a57b9f832a2bb7de6
Austin Wright <awguitar86@gmail.com>
2018-02-12 14:20:26 -0700

    changes to notes

--- ChatServer-1/server/controllers/messages_controller.js
+++ ChatServer-1/server/controllers/messages_controller.js
@@ -1,12 +1,12 @@
-var messages = [];  //Create an array to hold the messages.
-var id = 0;         //Create a variable that will keep track of what id to assign to messages. The id should start at 0 and increment after every creation.
+var messages = [];                          //Create an array to hold the messages.
+var id = 0;                                 //Create a variable that will keep track of what id to assign to messages. The id should start at 0 and increment after every creation.
 
-module.exports = {  //Export an object with methods to create, read, update, and delete messages. Each with two prameters req and res.
+module.exports = {                          //Export an object with methods to create, read, update, and delete messages. Each with two prameters req and res.
     create: ( req, res ) => {
-        const { text, time } = req.body;  //create a new message object using text and time from the request body and also the global id variable.
-        messages.push({ id, text, time }); //It should then push this new messsage object into the messages array.
-        id++;  //increment id by one.
-        res.status(200).send( messages ); //send the updated messages array.
+        const { text, time } = req.body;    //Create a new message object using text and time from the request body and also the global id variable.
+        messages.push({ id, text, time });  //It should then push this new messsage object into the messages array.
+        id++;                               //Increment id by one.
+        res.status(200).send( messages );   //Send the updated messages array.
     },
     read: ( req, res ) => {
 
